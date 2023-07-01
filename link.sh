dd9b5d1ad170e4eaac034cb4aac378b162034ee2
Josh Elliott <josh.roy.elliott@gmail.com>
2023-07-02 09:41:41 +1000

    Fixed dot syntax

--- link.sh
+++ link.sh
@@ -5,7 +5,7 @@ symlinked_files=("zshrc" "tmux.conf")
 
 for file in "${symlinked_files[@]}"; do
     dotfile_path="$dotfiles_dir/$file"
-    symlink_path="$HOME/$file"
+    symlink_path="$HOME/.$file"
 
     # Check if the file exists and is not a symlink
     if [[ -e "$symlink_path" && ! -L "$symlink_path" ]]; then
@@ -20,6 +20,6 @@ for file in "${symlinked_files[@]}"; do
     fi
 
     # Create the symbolic link
-    ln -s "$dotfile_path" ."$symlink_path"
-    echo "Created symlink for '$dotfile_path' in .'$symlink_path'"
+    ln -s "$dotfile_path" "$symlink_path"
+    echo "Created symlink for '$dotfile_path' in '$symlink_path'"
 done
